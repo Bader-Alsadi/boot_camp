370278161bdc8a2268da3ec671d018c8493c9223
Bader Alsadi <baderalsady@gmaile.com>
2023-09-13 16:47:40 +0300

    defualt

--- 12.join_to_string.dart
+++
@@ -1,9 +0,0 @@
-void main(List<String> args) {
-  List<String> str1 = ["bader", "saleh", "bader", "alsadi"];
-
-  print(join_string(str1));
-}
-
-String join_string(List<String> str) {
-  return str.join(" ");
-}
--- map_task.dart
+++
@@ -1,5 +0,0 @@
-void main(List<String> args) {
-  List l1 = [2, 5, 10, 30, 60];
-  List result = l1.map((e) => e * 2).toList();
-  print(result);
-}
