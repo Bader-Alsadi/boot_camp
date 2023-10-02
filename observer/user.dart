d489a2e18f1ff3603026a78bdfd62b75a501464c
Bader Alsadi <baderalsady@gmaile.com>
2023-10-02 15:25:57 +0300

    day

--- observer/user.dart
+++
@@ -1,9 +0,0 @@
- class User {
-
-  late String name;
-  User({required this.name});
-  void coffeeNot ({required DateTime dateTime ,required String message}){
-
-    print("[${dateTime}] Hey $name $message");
-  }
-}
