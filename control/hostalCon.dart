0e47b55f7cb407287b5170ad842d411ca34eff1d
Bader Alsadi <baderalsady@gmaile.com>
2023-10-02 15:48:21 +0300

    dsdcs

--- control/hostalCon.dart
+++ control/hostalCon.dart
@@ -2,25 +2,35 @@ import '../models/Student.dart';
 import 'StudentCon.dart';
 
 class HostalCon {
-  checkin(){
+  checkin() {
     print("Checkin");
   }
 
-  checkout(){
+  checkout() {
     print("Checkout");
   }
-ShowDetails({required int id}){
-  List<Student> students=StudentControler.studenList;
-     int index =students.indexWhere((element) => element.id == id);
-    if (index != -1){
-print("""
+
+  payFees({required int id_std, required double fees}) {
+    List<Student> students = StudentControler.studenList;
+    int index = students.indexWhere((element) => element.id == id);
+    if (index != -1) {
+      students[index].hostel!.fees = fees;
+      students[index].hostel!.paid_status = true;
+    } else
+      print("not found");
+  }
+
+  ShowDetails({required int id}) {
+    List<Student> students = StudentControler.studenList;
+    int index = students.indexWhere((element) => element.id == id);
+    if (index != -1) {
+      print("""
 Tyoe:${students[index].hostel!.type}
 room:${students[index].hostel!.room}
 block:${students[index].hostel!.block}
 state piad:${students[index].hostel!.paid_status ? "paid" : "unpaid"}
 """);
-    }
-    else
+    } else
       print("Not found");
-}
+  }
 }
